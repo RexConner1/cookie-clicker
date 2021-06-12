32297e1fc0eda6bc3f6d138d7a6bcc354bc0891a
David Orona <oronadavid@gmail.com>
2021-06-11 19:58:34 -0500

    Transfer version and beta

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React, { Component } from "react";
 import './App.css';
 import TopBar from './components/TopBar/TopBar'
 
+const VERSION=2.031;
+const BETA=0;
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      version: VERSION,
+      beta: BETA
     }
   }
 
